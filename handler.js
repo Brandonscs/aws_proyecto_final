ea1562a3160193479e1aaf2a958f72de8ec43263
Brandonscs <brandoncarvajal2002@gmail.com>
2023-09-10 19:45:43 -0500

    se configura el envio de email a los clientes

--- handler.js
+++ handler.js
@@ -5,6 +5,7 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 const sqs = new AWS.SQS();
+const ses = new AWS.SES({ region: 'us-east-1' });
 
 const conexion = mysql.createConnection({
   host: 'aws-proyectodfinal-cb.c9jwd0itecy9.us-east-1.rds.amazonaws.com',
@@ -26,13 +27,50 @@ module.exports.crearPedido = async (event) => {
       }
     });
   });
+  // Consultar la base de datos para obtener el correo electrónico del cliente
+  const queryClienteEmail = "SELECT nombre_completo, correo_electronico FROM clientes WHERE id = ?;";
+  let clienteNombre = '';
+  let clienteEmail = '';
+  
+  await new Promise((resolve, reject) => {
+    conexion.query(queryClienteEmail, [pedido.cliente_id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (results && results.length > 0) {
+          clienteNombre = results[0].nombre_completo;
+          clienteEmail = results[0].correo_electronico;
+        }
+        resolve();
+      }
+    });
+  });
+
+  // Consultar la base de datos para obtener el nombre y valor del producto
+  const queryProducto = "SELECT nombre_producto, valor_producto FROM productos WHERE id = ?;";
+  let producto_nombre = '';
+  let producto_valor = 0;
+
+  await new Promise((resolve, reject) => {
+    conexion.query(queryProducto, [pedido.producto_id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (results && results.length > 0) {
+          producto_nombre = results[0].nombre_producto;
+          producto_valor = results[0].valor_producto;
+        }
+        resolve();
+      }
+    });
+  });
   
   const timestamp = new Date().getTime();
   const filename = `pedido_${timestamp}.json`;
 
   const pedidoData = {
-    Cliente: pedido.cliente_id,
-    Producto: pedido.producto_id,
+    Cliente: clienteNombre,
+    Producto: producto_nombre,
     Cantidad: pedido.cantidad,
     "Valor Total": pedido.valor_total,
   };
@@ -51,13 +89,33 @@ module.exports.crearPedido = async (event) => {
 
   await sqs.sendMessage(params).promise();
 
+  // Enviar correo electrónico al cliente
+  const paramsEmail = {
+    Source: "brandon.carvajal27520@ucaldas.edu.co",
+    Destination: {
+      ToAddresses: [clienteEmail],
+    },
+    Message: {
+      Subject: {
+        Data: "Detalles del pedido",
+      },
+      Body: {
+        Text: {
+          Data: `Detalles del pedido:\n\nCliente: ${clienteNombre}\nProducto: ${producto_nombre}\nValor unitario: ${producto_valor}\nCantidad: ${pedido.cantidad}\nValor Total: ${pedido.valor_total}`,
+        },
+      },
+    },
+  };
+
+  await ses.sendEmail(paramsEmail).promise();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "Pedido creado con exito",
-        Cliente: pedido.cliente_id,
-        Producto: pedido.producto_id,
+        Cliente: clienteNombre,
+        Producto: producto_nombre,
         Cantidad: pedido.cantidad,
         "Valor Total": pedido.valor_total,
       },
